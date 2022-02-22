1e68cb440168dd3e9c5f4d48a5322e290be0b5ab
Sam Lyu <slyu@clearpath.ai>
2022-02-22 17:31:06 -0500

    Add ripgrep for fzf in vim

--- setup.sh
+++ setup.sh
@@ -2,6 +2,9 @@
 
 CURR_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+# Install ripgrep
+sudo apt-get install ripgrep
+
 # Install vim and vim-plug
 apt install vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
